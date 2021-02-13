021db2cef068f14bb04da051487fe602be2ae12b
raulynas <laurynas@inwoodengineering.co.uk>
2021-02-13 14:11:04 +0100

    alpgabet position

--- alphabetPosition1.js
+++ alphabetPosition1.js
@@ -10,6 +10,3 @@ function alphabetPosition(text) {
     return newText.trim();
 }
 console.log(alphabetPosition('sdaf*!_#^'));
-
-// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
-// Should return "21 9 6 20 22 15 20 6 21 20 6 21 20 2 21 21 24 6 13 23 6 16 4 13 16 4 12" (as a string)
