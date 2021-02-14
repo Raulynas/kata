e5ba6cd23ef12d517c2f3e683cdf2eddb535dc1b
raulynas <laurynas@inwoodengineering.co.uk>
2021-02-14 22:11:03 +0100

    Test

--- test.js
+++ test.js
@@ -1,32 +1,5 @@
-function persistence(num) {
-    if (num < 10) {
-        return 0;
-    }
-    let count = 1;
-
-    function result(text) {
-        let sum = 1;
-        let string = text.toString();
-        for (let i = 0; i < string.length; i++) {
-            sum *= string[i];
-        }
-        return sum;
-    }
-
-    let total = num;
-
-    while (result(total) > 9) {
-        total = result(total);
-        count++;
-    }
-    return count;
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((a, b) => a + b);
 }
 
-console.log(persistence());
-
-//  describe('Initial Tests', function () {
-//     Test.assertEquals(persistence(39),3);
-//     Test.assertEquals(persistence(4),0);
-//     Test.assertEquals(persistence(25),2);
-//     Test.assertEquals(persistence(999),4);
-//   });
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
